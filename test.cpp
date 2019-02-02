d217e84022359ddb05fda208cd4c5f4ad847e36c
ankitaaaaaaaaaa <47266075+ankitaaaaaaaaaa@users.noreply.github.com>
2019-02-02 11:42:27 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/*int inc(int i){
+	static int c=0;
+	c=c+i;
+	return(c);
+}
+int main(){
+	int i,j;
+	for(i=0;i<=4;i++){
+		j=inc(i);
+	}
+	printf("%d",j);
+	return 0;
+}
+int main(){
+	int k=35, *z, *y;
+	y=z=&k;
+	printf("k = %d z = %p y = %p",k,z,y);
+	*z++= *y--;
+	k++;
+	printf("k = %d z = %p y = %p",k,z,y);
+	return 0;
+}*/
+int main(){
+	int arr[1]={ 10 };
+	printf(" the value is %d", arr[1]);
+	return 0;
+}
